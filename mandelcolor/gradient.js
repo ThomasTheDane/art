08b9b7b2d860abcb4c3874df2c840e15f6737bc9
Thomas Nattestad <thomas.nattestad@gmail.com>
2018-03-17 13:56:23 -0700

    removed extra outputs and added mandelcolor

---
+++ mandelcolor/gradient.js
@@ -0,0 +1,53 @@
+class Gradient{
+    constructor(gradientColors = [{color:[255, 255, 255], position: 0.0}, {color:[0, 0, 0], position: 1.0}]){
+        if(gradientColors[0].position != 0 || gradientColors[gradientColors.length - 1].position != 1.0){
+            throw "Error: gradient must have element positioned at 0 and 1"
+        }
+        this.colors = gradientColors;
+    }
+
+    getColor(atPosition){
+        if(atPosition < this.colors[0].position || atPosition > this.colors[this.colors.length - 1].position){
+            throw 'Error: requested position out of range';
+        }
+        let color1 = [];
+        let color2 = [];
+        let ratio;
+
+        for(let i = 1; i < this.colors.length; i++){
+            if(atPosition <= this.colors[i].position){
+                color1 = this.colors[i-1].color;
+                color2 = this.colors[i].color;
+                ratio = (atPosition - this.colors[i-1].position) / (this.colors[i].position - this.colors[i-1].position);
+                break;
+            }
+        } 
+
+        return this.mixColors(color2, color1, ratio);
+    }
+
+    mixColors(color1, color2, ratio){
+        let antiRatio = 1 - ratio;
+        let mix = [(color1[0] * ratio) + (color2[0] * antiRatio),
+                   (color1[1] * ratio) + (color2[1] * antiRatio),
+                   (color1[2] * ratio) + (color2[2] * antiRatio)];
+        // console.log(`${color1} and ${color2} mix at ratio ${ratio} to make ${mix}`)
+        return mix;
+    }
+
+    drawGradientOnCanvas(canvas){
+        let context = canvas.getContext("2d");
+        let width = canvas.width;
+        let height = canvas.height;
+        for(let i = 0; i <= width; i++){
+            let mixed = aGradient.getColor(i / width);    
+            context.fillStyle = aGradient.rgbString(mixed);
+            context.fillRect(i, 0, 1, height);
+        }
+    }
+
+    rgbString(color){
+        return `rgb(${Math.round(color[0])}, ${Math.round(color[1])}, ${Math.round(color[2])})`;
+        
+    }
+}
---
+++ mandelcolor/mandelbrot.js
@@ -0,0 +1,66 @@
+class Mandelbrot{
+    // constructor(width, height, {xmin, xmax, ymin, ymax} = {xmin: -2, xmax: 1, ymmin: -1, ymax: 1}){
+    constructor(width, height, {xmin = -2, xmax = 1, ymin = -1, ymax=1} = {}){
+        console.log(`instantiating mandelbrot resolution ${height} by ${width} spanning x ${xmin} to ${xmax} and y ${ymin} to ${ymax}`);
+
+        //set basic parameter to object
+        this.width = width;
+        this.height = height;
+        this.xmin = xmin;
+        this.xmax = xmax;
+        this.ymin = ymin;
+        this.ymax = ymax;
+        
+        //initialize the nested array to store recursive depth for each pixel 
+        this.depth = new Array(width);
+        for(let i = 0; i < width; i++){
+            this.depth[i] = new Array(height);
+        }
+    }
+
+    computeMandelbrot({maxIterations = 500}){
+        console.log(`max iterationsmax: ${maxIterations}`);
+        var dx = (this.xmax - this.xmin) / this.width;
+        var dy = (this.ymax - this.ymin)/ this.height;
+
+        let y = this.ymin;
+        for (let j = 0; j < this.height; j++) {
+            let x = this.xmin;
+            for (let i = 0; i < this.width; i++) {
+                let a = 0 + x
+                let b = 0 + y;
+
+                let n = 0;
+                while(n < maxIterations){
+                    let aa = a * a;
+                    let bb = b * b;
+                    let twoab = 2 * a * b;
+                    a = aa - bb + x;
+                    b = twoab + y;
+                    if(aa+bb > 16){
+                        break;
+                    }
+                    n += 1;
+                }
+                this.depth[i][j] = n;
+                x += dx;
+            }
+            y += dy;
+        }
+    }
+
+    drawMandebrotlOnCanvas(canvas, maxIterations, gradient = new Gradient()){
+        this.computeMandelbrot({maxIterations: maxIterations});
+        var context = canvas.getContext("2d");
+        for (let j = 0; j < this.height; j++) {
+            for (let i = 0; i < this.width; i++) {
+                if(this.depth[i][j] == maxIterations){
+                    context.fillStyle = `rgb(0, 0, 0)`;
+                }else{
+                    context.fillStyle = gradient.rgbString(gradient.getColor(this.depth[i][j] / 500)) //`rgb(${this.depth[i][j]}, ${this.depth[i][j]}, ${this.depth[i][j]})`;
+                }
+                context.fillRect(i, j, 1, 1);
+            }
+        }
+    }
+}
