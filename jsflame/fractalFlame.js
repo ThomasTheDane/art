cb1248dfe533e385aee6045dba24e846a95f5bd9
Thomas Nattestad <thomas.nattestad@gmail.com>
2018-08-11 14:29:43 -0700

    it works

--- jsflame/fractalFlame.js
+++ jsflame/fractalFlame.js
@@ -23,7 +23,7 @@
 //     }
 // ];
 
-
+//todo change to transform
 function pickVariation(variations){
     //sum weight. Note these are the weights of each variation, not the weight of the transform 
     let totalWeight = variations[0].weight;
@@ -53,63 +53,87 @@ allVariations = {
 transform1 = {  variations: [{name: 'linear', weight:1.0},
                             ],
                 params: {a: 0.5,
-                        b: 0,
-                        c: 0,
-                        d: 0.5,
-                        e: 0,
-                        f: 0
+                         b: 0,
+                         c: 0,
+                         d: 0,
+                         e: 0.5,
+                         f: 0
                         }
              };
 transform2 = {  variations: [{name: 'linear', weight:1.0},
                             ],
                 params: {a: 0.5,
-                        b: 0,
-                        c: 0.5,
-                        d: 0.5,
-                        e: 0,
-                        f: 0
+                         b: 0,
+                         c: 0.5,
+                         d: 0,
+                         e: 0.5,
+                         f: 0
                         }
              };
 
 transform3 = {  variations: [{name: 'linear', weight:1.0},
                             ],
                 params: {a: 0.5,
-                        b: 0,
-                        c: 0.25,
-                        d: 0.5,
-                        e: 0,
-                        f: 0.5
+                         b: 0,
+                         c: 0,
+                         d: 0,
+                         e: 0.5,
+                         f: 0.5
                         }
 };
 
+// transforms = [transform1, transform2, transform3];
+// function dumbIFS(){
+//     for(let i = 0; i < 10; i++){
+//         let point = [(Math.random()) * 2 - 1, (Math.random()) * 2 - 1]
+//         let point2 = point;
+        
+//         for(let j = 0; j < 50; j++){
+//             console.log("before: ", point, point2);
+//             let chosen = getRandomInt(3);
+//             let func = triangleFunctions[chosen];
+//             point = func(point);
+            
+//             let transform = transforms[chosen];
+//             point2 =  [(transform.params.a * point2[0]) + (transform.params.b * point2[1]) + (transform.params.c), 
+//                        (transform.params.d * point2[0]) + (transform.params.e * point2[1]) + (transform.params.f) ];
 
-function IFS(point, transform){
-    for (let j = 0; j < 30; j++) {       
-        
+            
+//             console.log("funcs: ", func, transform.params);
+//             console.log("after: ", point, point2);
 
-        // point = {x: aTransform. (transform.params.a * point.params.x) + (transform.params.b * point.params.y) + (transform.params.c), 
-        //          y: (transform.params.d * point.params.x) + (transform.params.e * point.params.y) + (transform.params.f) };
-        
-        //need to randomly pick a transform based on variations weight 
-        let func = allVariations[pickVariation(transform.variations).name]; //note: this is very inefficient to iterate over given the structure of calculating the probabilities of picking a variation 
-        
+//             if(j > 20){
+//                 // paintPointToCanvas({x: point[0], y: point[1]});
+//                 paintPointToCanvas({x: point2[0], y: point2[1]});
+//             }
+//         }
+//     }
+// }
+
+function IFS(point, transforms){
+    for (let j = 0; j < 500; j++) {       
+
+        //need to randomly pick a transform
+        let transform = randomItem(transforms);
+
+        //todo: we need to sum all of the variations of each transform and multiply each variation function result by the weight of the variation
+        // let func = allVariations[pickVariation(transform.variations).name]; 
+
         //we morph the point based on params and put it through the chosen function 
         point =  {x: (transform.params.a * point.x) + (transform.params.b * point.y) + (transform.params.c), 
                   y: (transform.params.d * point.x) + (transform.params.e * point.y) + (transform.params.f) };
-        point = func(point);
+        // point = func(point);
 
         if(j > 20){ 
             paintPointToCanvas(point);
+            // console.log([point.x * 500, point.y * 500])
         }
     }
 }
 
-// let canvas = document.getElementById("mainCanvas");
-
-// for (let i = 0; i < 1000; i++) {
-//     let point = [(Math.random()) * 2 - 1, (Math.random()) * 2 - 1]
-//     IFS(point);
-// }
+function randomItem(items){
+    return items[Math.floor(Math.random()*items.length)];
+}
 
 function paintPointToCanvas(point){
     let canvas = document.getElementById('mainCanvas');
@@ -117,10 +141,9 @@ function paintPointToCanvas(point){
     context.fillStyle = `rgb(0, 0, 0)`;
 
     let convertedPoint = [(point.x * (canvas.width / 2)) + (canvas.width / 2), 
-                          (point.y * (canvas.height / 2)) + (canvas.height / 2), 
+                          (point.y * (canvas.height / 2)) + (canvas.height / 2)
                          ];
 
-    console.log(convertedPoint);
     context.fillRect(convertedPoint[0], convertedPoint[1], 1, 1);
     // console.log(convertedPoint);
 }
@@ -130,18 +153,15 @@ function getRandomInt(max) {
 }
 
 function drawAndCalculateFlame(transforms){
-    for (const aTransform of transforms) {
-        for (const aVariation of aTransform.variations) {
-            for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 200; i++) {
 
-                let point = {x:(Math.random()) * 2 - 1, y: (Math.random()) * 2 - 1}
-                IFS(point, transform1);
+        let point = {x:(Math.random()) * 2 - 1, y: (Math.random()) * 2 - 1}
+        IFS(point, transforms);
 
-            }
-        }
     }
 }
 
 drawAndCalculateFlame([transform1, transform2, transform3]);
+// dumbIFS();
   //todos
-  //probability weights to functions 
+  //probability weights to transforms  
